3a226a380bd2558bb890e61e711858da934b368c
vkittycodes <68565027+vkittycodes@users.noreply.github.com>
2020-07-20 18:16:52 -0400

    added JavaScript function

---
+++ script.js
@@ -0,0 +1,5 @@
+function changemagicaldivness()
+{
+    alert('Oof. This is not a Minecraft link. It is above this message...    Or is it?');
+    document.getElementById('magicaldivness').style.backgroundColor='white';
+}
